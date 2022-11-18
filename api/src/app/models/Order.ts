4f3d2646d90a60ebd3c93e5eb66acfb9ac8cae97
Matheus Freitas <matheusbfreitas@gmail.com>
2022-11-17 21:11:45 -0300

    Fix order model and add itens in db

--- api/src/app/models/Order.ts
+++ api/src/app/models/Order.ts
@@ -17,12 +17,12 @@ export const Order = model('Order', new Schema({
         type: Schema.Types.ObjectId,
         required: true,
         ref: 'Product'
-      }
+      },
+      quantity: {
+        type: Number,
+        default: 1,
+      },
     }]
-  },
-  quantity: {
-    type: Number,
-    default: 1,
   },
   createdAt: {
     type: Date,
